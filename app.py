684c817960f1f28f78f934f8f728e8f4171a3d0a
MKNITHEESH-12 <nitheeshmk123@gmail.com>
2026-01-16 00:15:58 +0530

    first commit

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ def chat():
     except Exception as e:
         return jsonify({"answer": f"⚠️ Error: {str(e)}"}), 500
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
